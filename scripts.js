60be862b8e08cf41c757eea931845a69997490cd
Fireor <128834950+Fireor@users.noreply.github.com>
2024-02-22 10:16:32 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const cursors = document.querySelectorAll('.cursor');
-
-    document.addEventListener('mousemove', function(e) {
-        cursors.forEach(cursor => {
-            cursor.style.left = e.pageX + 'px';
-            cursor.style.top = e.pageY + 'px';
-        });
-    });
-});
 
