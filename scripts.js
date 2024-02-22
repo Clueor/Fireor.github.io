d48ea31c9c36b2c498574db96acf940cbfded814
Fireor <128834950+Fireor@users.noreply.github.com>
2024-02-22 18:04:12 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,29 +1,110 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const box = document.querySelector('.box');
-    const mouse = {
-        _x:  0,
-        _y:  0,
-        x:  0,
-        y:  0,
-        updatePosition: function(event) {
-            this.x = event.clientX - this._x;
-            this.y = event.clientY - this._y;
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
         },
-        setOrigin: function(x, y) {
-            this._x = x;
-            this._y = y;
+        "color": {
+            "value": "#ffffff"
         },
-        getX: function() {
-            return this.x;
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "",
+                "width": 100,
+                "height": 100
+            }
         },
-        getY: function() {
-            return this.y;
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "top",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": true,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
         }
-    };
-
-    document.addEventListener('mousemove', function(e) {
-        mouse.updatePosition(e);
-        box.style.transform = `translate(${mouse.getX()}px, ${mouse.getY()}px)`;
-        mouse.setOrigin(e.clientX, e.clientY);
-    });
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "grab"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 150,
+                "line_linked": {
+                    "opacity": 0.75
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
 });
