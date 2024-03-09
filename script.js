f2ed9b573fb21d804bdb4bb2df5dc063c8084f5b
vihaan <82585596+vihaan69@users.noreply.github.com>
2024-03-09 20:35:51 +0530

    create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function windowOpen() {
+  window.open('https://vote.pika-network.net/1', '_blank');
+  window.open('https://vote.pika-network.net/2', '_blank');
+  window.open('https://vote.pika-network.net/3', '_blank');
+  window.open('https://vote.pika-network.net/4', '_blank');
+  window.open('https://vote.pika-network.net/5', '_blank');
+  window.open('https://vote.pika-network.net/6', '_blank');
+  window.open('https://vote.pika-network.net/7', '_blank');
+  console.log("Opening links...");
+}
